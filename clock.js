84d5fbe893b95a6b86bf29b28e8c37e3bae9e01d
256bitguy <ashishrajsri2233@gmail.com>
2023-04-21 21:52:36 +0530

    First commit

---
+++ clock.js
@@ -0,0 +1,17 @@
+const SecondHand=document.querySelector('.second-hand')
+const minuteHand=document.querySelector('.minute-hand')
+const hourHand=document.querySelector('.hour-hand')
+function setDate(){
+    const now=new Date();
+    const seconds=now.getSeconds();
+    const minutes=now.getMinutes();
+    const hours=now.getHours();
+    const SecondsDegrees =(seconds/60)*360+90;
+    const MinutesDegrees=(minutes/60)*360+90;
+    const HoursDegrees=(hours/60)*360+90;
+    console.log(seconds);
+    SecondHand.style.transform=`rotate(${SecondsDegrees}deg)`;
+    minuteHand.style.transform=`rotate(${MinutesDegrees}deg)`
+    hourHand.style.transform=`rotate(${HoursDegrees}deg)`
+}
+setInterval(setDate,1000);
